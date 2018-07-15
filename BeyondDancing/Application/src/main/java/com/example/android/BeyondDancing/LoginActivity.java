fc9885333ffc3032bcbde29df799d2cf56b4980a
Tony Zhang <tzhang995@gmail.com>
2018-07-15 14:29:06 -0400

    Did stuff

---
+++ BeyondDancing/Application/src/main/java/com/example/android/BeyondDancing/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.BeyondDancing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
