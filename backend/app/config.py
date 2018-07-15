f01cdf981581867e4f9b66a532096283219ea3fa
Hong Phuc Dao <hpdao@uwaterloo.ca>
2018-07-15 18:08:38 -0400

    google sign in

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,11 @@
 import os
+import json
+
+CLIENT_SECRETS_PATH = "/Users/david/Projects/beyond-dancing/backend/app/client_secrets.json"
+CLIENT_ID = json.loads(
+    open(CLIENT_SECRETS_PATH, 'r').read())['web']['client_id']
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:coboman@/beyond_dancing'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
--- backend/app/models.py
+++ backend/app/models.py
@@ -3,10 +3,10 @@ from app import app
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username) + 'id: ' + id
+        return '<User %r>' % self.email
 
 
