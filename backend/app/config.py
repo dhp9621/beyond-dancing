bacba7585764aa03c90ef3386c3000841cc0db07
Hong Phuc Dao <hpdao@uwaterloo.ca>
2018-07-16 14:23:20 -0400

    fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,6 +6,6 @@ CLIENT_ID = json.loads(
     open(CLIENT_SECRETS_PATH, 'r').read())['web']['client_id']
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:coboman@/beyond_dancing'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://david:coboman@/beyond_dancing'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
